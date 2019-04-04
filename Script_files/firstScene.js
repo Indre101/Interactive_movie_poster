5b44c61c10e33174c3196ca03e330039d4bc2bb8
indre101 <zygaityte.indre@gmail.com>
2019-04-04 21:28:00 +0200

    added turning around to

--- Script_files/firstScene.js
+++ Script_files/firstScene.js
@@ -2,17 +2,18 @@ const key = document.getElementById("key");
 const bg = document.getElementById("container");
 const btn1 = document.getElementById("start");
 const audioBegining = document.getElementById("audioBegining");
+const boy = document.getElementById("boy");
 
 key.style.display = "none";
 document.getElementById("twins").style.display = "none";
 document.getElementById("boy").style.display = "none";
 
-
+boy.classList.add("boy1");
 
 btn1.onclick = function () {
   key.style.display = "block";
   document.getElementById("twins").style.display = "block";
-  document.getElementById("boy").style.display = "block";
+  boy.style.display = "block";
   btn1.style.display = "none";
   document.getElementById("instructions").style.display = "none";
   bg.removeChild(audioBegining);
@@ -27,14 +28,15 @@ key.onclick = function () {
   key.style.display = "none";
 
   bg.classList.add("fadeout");
+  boy.classList.remove("boy1");
 
 
   setInterval(() => {
     bg.classList.remove("container1");
+    boy.classList.add("boy2");
 
     bg.classList.add("container2");
 
   }, 1200);
-
 
 }
