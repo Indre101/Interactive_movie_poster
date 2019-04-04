f68c9ee9e7d378ba378ae111dc8a8c3e6d10dccb
indre101 <zygaityte.indre@gmail.com>
2019-04-04 09:20:45 +0200

    setting function for repeating

--- Script_files/fighting.js
+++ Script_files/fighting.js
@@ -2,12 +2,26 @@ const jack = document.getElementById("jack");
 
 function jackMove() {
   jack.classList.add("jackWalking");
-
   setTimeout(() => {
     jack.classList.remove("jackWalking");
     jack.classList.add("jackJump");
   }, 4000);
 
+  setTimeout(() => {
+
+    jack.classList.remove("jackWalking");
+    jack.classList.remove("jackJump");
+    jack.classList.add("shaking");
+
+  }, 8000);
+
+
 }
 
-jackMove()
+let i = 0;
+
+if (i < 5) {
+  i++;
+  console.log(i);
+  jackMove();
+}
