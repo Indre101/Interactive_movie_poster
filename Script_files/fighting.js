e8e398d48e3ff7e1fe0faad24dc9374f6cfa7b91
indre101 <zygaityte.indre@gmail.com>
2019-04-04 18:27:41 +0200

    fixed few bugs

--- Script_files/fighting.js
+++ Script_files/fighting.js
@@ -6,10 +6,16 @@ const hitting = document.getElementById("hitting");
 const blood = document.querySelectorAll(".blood");
 // audio control
 let audio1 = new Audio('././audio/bash_your_brains_in.mp3');
-audio1.play();
+
+
+
+window.onload = (event) => {
+  audio1.play();
+
+};
+
 
 ///audio controll
-
 
 
 let i = 0;
@@ -18,40 +24,43 @@ jack.classList.add("jackWalking");
 
 function jackMove() {
   i++
+
   setTimeout(() => {
     jack.classList.remove("jackWalking");
     jack.classList.add("jackJump");
   }, 4000);
 
   setTimeout(() => {
-
-
+    console.log(i);
     jack.classList.remove("jackWalking");
     jack.classList.remove("jackJump");
     jack.classList.add("shaking");
+
     if (i === 2) {
       jack.classList.remove("shaking");
       jack.classList.add("running2");
-      console.log(i);
 
     }
   }, 8000);
 
 }
-
 jackMove();
 
 
-wendy.classList.add("crying1");
 wendy.addEventListener("animationend", wendyListener, false);
+wendy.classList.add("crying1");
 
 function wendyListener(event) {
+
   switch (event.type) {
     case "animationend":
       i++;
+
       wendy.classList.remove("crying1");
       wendy.style.backgroundImage = "url(././images/fighting/wendy-06_back.png)"
       wendy.classList.add("crying2");
+
+
       if (i === 3) {
         wendy.style.backgroundImage = ""
         wendy.classList.remove("crying2");
@@ -68,6 +77,7 @@ function wendyListener(event) {
 bat.onclick = function () {
   changeBg()
   setTimeout(() => {
+    hitting.classList.add("hittinAnimation1");
     hitting.style.display = "block";
     setTimeout(() => {
       hitting.style.backgroundImage = "url('././images/wendyjack-03.png')";
@@ -77,12 +87,11 @@ bat.onclick = function () {
       })
 
     }, 5000);
+
     container.style.display = "none";
-    let audio = new Audio('././audio/give_me_the_bat_wendy_ahhh_god_damm_it_bang.mp3');
-    audio.play();
+    const audio2 = new Audio('././audio/give_me_the_bat_wendy_ahhh_god_damm_it_bang.mp3');
+    audio2.play();
     audio1.pause();
-
-
 
   }, 4800);
 }
@@ -91,4 +100,27 @@ function changeBg() {
 
   container.classList.add("zoomin");
 
+}
+
+// blood elevator appear
+
+const blood3 = document.getElementById("blood3"); // finishes animation
+const elevator = document.getElementById("elevator"); //elevator appears
+
+blood3.addEventListener("animationend", bloodAppear, false);
+
+function bloodAppear(event) {
+  switch (event.type) {
+    case "animationend":
+
+      setTimeout(() => {
+        hitting.style.display = "none";
+        elevator.style.display = "block";
+
+      }, 3000);
+
+
+      break;
+
+  }
 }
